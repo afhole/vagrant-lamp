77654ec44fb808a9cb96e97d3ad6cf6e73a0366b
Miguel <mordonez.sanchez@gmail.com>
2013-07-16 13:37:57 +0200

    Update default.rb
    Error using last download_base i suggest to use for example http://astro-wise.org/instantclient
    
    Errno::ETIMEDOUT
    ----------------
    Error connecting to http://exodus.socsci.umn.edu/redhat/oracle-instantclient/instantclient-basic-linux.x64-11.2.0.3.0.zip - Connection timed out - connect(2)

--- cookbooks/oracle-instantclient/attributes/default.rb
+++ cookbooks/oracle-instantclient/attributes/default.rb
@@ -1,4 +1,4 @@
 default[:oracle_instantclient][:client_version] = "11.2.0.3.0"
 default[:oracle_instantclient][:client_dir_name] = "instantclient_11_2"
-default[:oracle_instantclient][:download_base] = "http://exodus.socsci.umn.edu/redhat/oracle-instantclient"
+default[:oracle_instantclient][:download_base] = "http://astro-wise.org/instantclient"
 default[:oracle_instantclient][:install_dir] = "/usr/local"
